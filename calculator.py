84366175a7512203fe4947649e301962515db2f8
gitnoti <58890805+gitnoti@users.noreply.github.com>
2021-05-03 14:56:13 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,42 @@
+pre_x = input("First number here: ")
+pre_y = input("Second number here: ")
+
+x = int(pre_x)
+y = int(pre_y)
+
+x1 = float(pre_x)
+y1 = float(pre_y)
+
+
+multiply = x * y
+divide = x // y
+add = x + y
+subtract = x - y
+exponent = x ** y
+
+multiply1 = x1 * y1
+divide1 = x1 // y1
+add1 = x1 + y1
+subtract1 = x1 - y1
+exponent1 = x1 ** y1
+print("")
+print("Simple:")
+print("Multiplied:", multiply, sep=(""))
+print("Divided:", divide, sep=(""))
+print("Added:", add, sep=(""))
+print("Subtracted:", subtract, sep=(""))
+print("Exponent:", exponent, sep=(""))
+print("")
+print("Complex:")
+print("Multiplied:", multiply1, sep=(""))
+print("Divided:", divide1, sep=(""))
+print("Added:", add1, sep=(""))
+print("Subtracted:", subtract1, sep=(""))
+print("Exponent:", exponent1, sep=(""))
+print("")
+print("")
+print("Scroll up to see results")
+print("code by noti")
+
+
+
